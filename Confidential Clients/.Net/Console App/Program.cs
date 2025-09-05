c2e8c1d132ea46c9f9abea25ec23bf40e164126d
Albert Starreveld <albertstarreveld@MacBook-Pro-van-Albert.local>
2025-09-05 20:38:08 +0200

    docs: Console-App demo

---
+++ Confidential Clients/.Net/Console App/Program.cs
@@ -0,0 +1,40 @@
+﻿using Duende.IdentityModel.Client;
+
+Console.WriteLine("Entrypage Console App Demo");
+Console.WriteLine("");
+
+Console.Write("authority (e.g. https://yourserver.sandbox.entrypage.io): ");
+var authority = Console.ReadLine();
+
+Console.Write("client_id: ");
+var clientId = Console.ReadLine();
+
+Console.Write("client_secret: ");
+var clientSecret = Console.ReadLine();
+
+var httpClient = new HttpClient();
+
+// Obtain the discovery document to find the location of the token-endpoint
+var discoveryDocument = await httpClient.GetDiscoveryDocumentAsync(authority);
+if (discoveryDocument.IsError)
+{
+    Console.Error.WriteLine("Failed to obtain discovery document: {discoveryDocument.Error}");
+    return;
+}
+
+// Post the client_id, the client_secret, and the scope to the token endpoint to obtain the token
+var token = await httpClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest()
+{
+    Address = discoveryDocument.TokenEndpoint,
+    ClientId = clientId,
+    ClientSecret = clientSecret,
+    Scope = "openid"
+});
+
+if (token.IsError)
+{
+    Console.Error.WriteLine($"Failed to obtain token: {token.Error}");
+    return;
+}
+
+Console.WriteLine($"Token: {token.AccessToken}");
